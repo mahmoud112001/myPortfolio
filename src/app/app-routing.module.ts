2d489b365b7bf2d9e26ba847ab9d6a7bffb36caf
mahmoud112001 <mahmoudawad112001@gmail.com>
2026-04-18 16:42:42 +0200

    refactor: replace static component tags with router-outlet for dynamic page routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './home/home.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { MyskillsComponent } from './myskills/myskills.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ContactMeComponent } from './contact-me/contact-me.component';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },             
+  { path: 'about', component: AboutMeComponent },    
+  { path: 'skills', component: MyskillsComponent },  
+  { path: 'projects', component: ProjectsComponent },
+  { path: 'contact', component: ContactMeComponent }, 
+  { path: '**', redirectTo: '' }                      
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
